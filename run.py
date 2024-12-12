3411a7c856aedbc17415507bb35dcf5695471a52
Akhil Anil <akhilanil.100@gmail.com>
2024-12-12 15:25:05 +0400

    New run.py

--- run.py
+++ run.py
@@ -4,5 +4,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Get the port from the environment
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    print(f"Starting app on port {port}...")
+    app.run(host="0.0.0.0", port=port, debug=True)
