f00a74de7335b0082545c29c241b9b3ae5434e96
Akhil Anil <akhilanil.100@gmail.com>
2024-12-12 15:22:09 +0400

    Modifying run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get the port from the environment
+    app.run(host="0.0.0.0", port=port)
